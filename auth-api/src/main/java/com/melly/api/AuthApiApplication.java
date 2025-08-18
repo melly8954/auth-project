7a26898667025a477e93eafbdd5bd8d5c2636d13
haily <49448330+melly8954@users.noreply.github.com>
2025-08-19 01:19:23 +0900

    feat: p6spy 기반 SQL 로깅 기능 추가 (250819)

--- auth-api/src/main/java/com/melly/api/AuthApiApplication.java
+++ auth-api/src/main/java/com/melly/api/AuthApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.melly"})
-@EnableJpaRepositories(basePackages = "com.melly.domain.repository")
-@EntityScan(basePackages = "com.melly.domain.entity")
+@EnableJpaRepositories(basePackages = "com.melly")
+@EntityScan(basePackages = "com.melly")
 public class AuthApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApiApplication.class, args);
